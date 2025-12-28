a39000bbac624f4f10aab8b8af650da6ec716a46
César Pérez <cesardanielperez@goodhabitz.com>
2025-12-28 21:00:21 +0100

    feat: Add new `favicon.png` and update meta tags for icon and apple-touch-icon.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import "../app.css";
-	import favicon from "$lib/assets/favicon.svg";
 
 	import MetaTags from "$lib/components/MetaTags.svelte";
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <MetaTags />
 
